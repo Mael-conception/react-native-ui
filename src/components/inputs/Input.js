e7e5b9dacf72d33d4b17e8072ef58b0e304b845a
vander makadi <vanotis720@gmail.com>
2024-06-27 22:49:51 +0200

    Implemented and styled input component

--- src/components/inputs/Input.js
+++ src/components/inputs/Input.js
@@ -1,11 +1,20 @@
-import React from 'react';
-import { TextInput, View, Text, TouchableOpacity } from 'react-native';
-import Styles from '../../styles/input.style'; 
+import React, { useState } from 'react';
+import { TextInput, View, Text } from 'react-native';
+import Styles from './input.style'; 
 import { useTheme } from '../../context/ThemeContext'; 
+import Icon from '../buttons/Icon'; 
 
-const Input = ({ label, placeholder, error, size = 'md', disabled, variant, iconLeft, iconRight }) => {
+const Input = ({ label, placeholder, error, size = 'md', disabled, variant, iconLeft, iconRight, secureTextEntry }) => {
     const { theme } = useTheme();
     const styles = Styles(theme);
+
+    const [isSecureTextEntry, setIsSecureTextEntry] = useState(secureTextEntry);
+
+    const toggleSecureEntry = () => {
+        setIsSecureTextEntry(!isSecureTextEntry);
+    };
+
+    const visibilityIcon = isSecureTextEntry ?'eye-off-outline': 'eye-outline'  ;
 
     return (
         <View style={styles.container}>
@@ -16,14 +25,23 @@ const Input = ({ label, placeholder, error, size = 'md', disabled, variant, icon
                 styles[variant],
                 disabled && styles.disabled
             ]}>
-                {iconLeft && <View style={styles.iconLeft}>{iconLeft}</View>}
+                {iconLeft && <Icon iconName={iconLeft} size={size} color={theme.text} style={styles.iconLeft} />}
                 <TextInput
                     style={[styles.input, styles[`input${size.toUpperCase()}`]]}
                     placeholder={placeholder}
-                    placeholderTextColor={theme.textSecondary}
+                    placeholderTextColor={styles.placeholder.color}
                     editable={!disabled}
+                    secureTextEntry={isSecureTextEntry && secureTextEntry} 
                 />
-                {iconRight && <View style={styles.iconRight}>{iconRight}</View>}
+                {iconRight && (
+                    <Icon
+                        iconName={iconRight === 'visibility' ? visibilityIcon : iconRight}
+                        size={size}
+                        color={theme.text}
+                        style={styles.iconRight}
+                        onPress={iconRight === 'visibility' ? toggleSecureEntry : null} 
+                    />
+                )}
             </View>
             {error && <Text style={styles.error}>{error}</Text>}
         </View>
